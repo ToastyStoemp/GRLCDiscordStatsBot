73ee0e912e8d323ed22f43c91ebadf687fa0f524
marzavec <admin@marzavec.com>
2018-01-29 21:33:40 -0800

    Cleaned up bot output with sexy embeds

--- main.js
+++ main.js
@@ -44,30 +44,62 @@ bot.on('message', msg => {
         } else {
             switch (cmd) {
                 case "help":
-                    if (arg == "")
-                        msg.reply("Bot made for **The Salt Mine** by *ToastyStoemp*\n List of commands:\n !help, !tip, !etaNextBlock, !tipRandomAdress, !stats");
-                    else {
-                        switch (arg) {
-                            case "help":
-                                msg.reply("Shows the help message");
-                                break;
-                            case "tip":
-                                msg.reply("Shows the adress of the server and bot dev, feel free to send us something :)");
-                                break;
-                            case "etanextblock":
-                                msg.reply("Calculates the estimate time till the next block will be mined. This is an estimate!");
-                                break;
-                            case "tiprandomadress":
-                                msg.reply("Shows the a random adress from someone currently mining in the pool, perfect if you want to surprise a fellow miner!");
-                                break;
-                            case "stats":
-                                msg.reply("Shows the some interesting stats about this pool!");
-                                break;
-                        }
-                    }
+                    msg.channel.send({
+                      "embed": {
+                        "description": "Bot made for **The Salt Mine** by *ToastyStoemp* & *Rut*\nList of commands:",
+                        "color": 16777215,
+                        "fields": [
+                          {
+                            "name": "!help",
+                            "value": "Shows this help message.",
+                            "inline": true
+                          },
+                          {
+                            "name": "!stats",
+                            "value": "Shows interesting stats about this pool!",
+                            "inline": true
+                          },
+                          {
+                            "name": "!tip",
+                            "value": "Shows the address of the server and bot devs, feel free to send us something :smile:"
+                          },
+                          {
+                            "name": "!etaNextBlock",
+                            "value": "Calculates an __estimated__ time until the next block will be mined. This is an estimate!"
+                          },
+                          {
+                            "name": "!tipRandomaddress",
+                            "value": "Shows a random address from someone currently mining in the pool- perfect if you want to surprise a fellow miner!"
+                          }
+                        ]
+                      }
+                    });
                     break;
                 case "tip":
-                    msg.reply("Feel free to tip **The Salt Mine** at: GWQC7SmUvvmkEbsNVrhBgm4hNMXxPUf3Sj\nOr throw some GRLC at the developer of this bot: GQFMXJCcnremXu8RAi89Hu8ivUUdd6EtVL");
+                    msg.channel.send({
+                      "embed": {
+                        "description": "Want to show your appreciation for all this salty goodness?",
+                        "color": 16777215,
+                        "fields": [
+                          {
+                            "name": "Pool Backbone ( Help keep us running! )",
+                            "value": "GWQC7SmUvvmkEbsNVrhBgm4hNMXxPUf3Sj"
+                          },
+                          {
+                            "name": "Spinax ( Our Server Stylist )",
+                            "value": "( Psst, we need your wallet address! )"
+                          },
+                          {
+                            "name": "ToastyStoemp ( Bot Developer #1 )",
+                            "value": "GQFMXJCcnremXu8RAi89Hu8ivUUdd6EtVL"
+                          },
+                          {
+                            "name": "Rut ( Bot Developer #2 )",
+                            "value": "GKxBoZ49srGXuGabXHdLEvRoq1uPQTX9wC"
+                          }
+                        ]
+                      }
+                    });
                     break;
                 case "etanextblock":
                     request({ url: "https://explorer.grlc-bakery.fun/api/getnetworkhashps", json: true }, function(error, response, body) {
@@ -81,23 +113,65 @@ bot.on('message', msg => {
 
                             var timePercentage = (Date.now() - config.block.last) / (24 * 60 * 60 * 1000);
                             var currentChance = timePercentage * dayChance;
-                            msg.reply(`With the current hashrate (${lastKnownPoolHashRateString}/s) compared to the global network hashrate (${Math.round(NetworkHashRate / 1000 / 1000 / 1000 * 100) / 100}GH/s) this pool has advanced ${Math.round(currentChance * 100) / 100}% towards a potential next block solve.\n This pool has makes up for ${(PoolHashRate / NetworkHashRate * 100)}‱ in the network.\nTo learn more about the math behind this calculation use !help etaNextBlock`);
+                            msg.reply(`with the current hashrate (${lastKnownPoolHashRateString}/s) compared to the global network hashrate of (${Math.round(NetworkHashRate / 1000 / 1000 / 1000 * 100) / 100}GH/s) this pool has advanced ${Math.round(currentChance * 100) / 100}% towards a potential next block solve.\n This pool has makes up for ${(PoolHashRate / NetworkHashRate * 100)}‱ in the network.\nTo learn more about the math behind this calculation use !help etaNextBlock`);
                         }
                     });
                     break;
-                case "tiprandomadress":
+                case "tiprandomaddress":
                     request({ url: "http://209.250.230.130/api/stats", json: true }, function(error, response, body) {
                         if (error) console.log(error);
                         else {
                             var garlicoinPool = body.pools.garlicoin
                             var obj_keys = Object.keys(garlicoinPool.workers);
                             var ran_key = obj_keys[Math.floor(Math.random() * garlicoinPool.workerCount)];
-                            msg.reply("Here is a random adress from the list of current workers: " + ran_key);
+                            msg.channel.send({
+                              "embed": {
+                                "description": "This guy is chipping away hard at that salt, lookin' for dat tasty bread! He could use a toasty warm tip :wink:",
+                                "color": 16777215,
+                                "fields": [
+                                  {
+                                    "name": "Address:",
+                                    "value": `${ran_key}`
+                                  },
+                                  {
+                                    "name": "Hash Rate:",
+                                    "value": `${garlicoinPool.workers[ran_key].hashrateString}`,
+                                    "inline": true
+                                  },
+                                  {
+                                    "name": "Shares:",
+                                    "value": `${garlicoinPool.workers[ran_key].shares}`,
+                                    "inline": true
+                                  }
+                                ]
+                              }
+                            });
                         }
                     });
                     break;
                 case "stats":
-                    msg.reply("Highest HashRate: " + config.stats.highestHashRate + "\nHighest Worker Count: " + config.stats.highestWorkercount + "\nWill show more stats soon.");
+                    msg.channel.send({
+                      "embed": {
+                        "description": "Zero bamboozle garlic salt stats!",
+                        "color": 16777215,
+                        "fields": [
+                          {
+                            "name": "Highest Pool Hashrate:",
+                            "value": `${config.stats.highestHashRate}`,
+                            "inline": true
+                          },
+                          {
+                            "name": "Highest Pool Workers:",
+                            "value": `${config.stats.highestWorkercount}`,
+                            "inline": true
+                          },
+                          {
+                            "name": "Want moar stats?",
+                            "value": "Send us some suggestions on what you'd like to see!"
+                          }
+                        ]
+                      }
+                    });
                     break;
             }
         }
